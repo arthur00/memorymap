7d8e8bce34562a9e8da6523a6b3d7b4ca406c19b
arthur00@gmail.com <arthur00@gmail.com@a1222fba-a338-a3e9-5d12-604b87a68814>
2011-05-25 00:42:03 +0000

    Some new changes. mmap actually allocates and returns now.

--- memmap/mm_main.c
+++ memmap/mm_main.c
@@ -28,18 +28,122 @@
 */
 
 #include "pub_tool_basics.h"
+#include "pub_tool_libcbase.h"
+#include "pub_tool_libcassert.h"
+#include "pub_tool_libcprint.h"
+#include "pub_tool_machine.h"      // VG_(fnptr_to_fnentry)
+#include "pub_tool_mallocfree.h"
+#include "pub_tool_options.h"
+#include "pub_tool_replacemalloc.h"
 #include "pub_tool_tooliface.h"
+#include "pub_tool_wordfm.h"
 
 ///////////////// Begin instrumenting stuff //////////////////////
 
 static
 void mm_mem_mmap ( Addr a, SizeT len, Bool rr, Bool ww, Bool xx, ULong di_handle )
 {
-        VG_(printf)("[0x%x]: %d\n",a,len);
+        VG_(printf)("Mmap: [%p]: %llu\n",a,len);
+}
+
+static
+void mm_new_mem_startup( Addr a, SizeT len,
+						Bool rr, Bool ww, Bool xx, ULong di_handle )
+{
+	VG_(printf)("MemStartup: [%p]: %llu\n",a,len);
+}
+
+static
+void mm_die_mem_munmap ( Addr a, SizeT len )
+{
+	VG_(printf)("Free: [%p]: %llu\n",a,len);
 }
 
 
 ///////////////// End instrumenting stuff ////////////////////////
+
+
+static
+void* new_block ( ThreadId tid, void* p, SizeT req_szB, SizeT req_alignB,
+				 Bool is_zeroed )
+{
+	tl_assert(p == NULL); // don't handle custom allocators right now
+	SizeT actual_szB, slop_szB;
+	
+	if ((SSizeT)req_szB < 0) return NULL;
+	
+	if (req_szB == 0)
+		req_szB = 1;  /* can't allow zero-sized blocks in the interval tree */
+	
+	// Allocate and zero if necessary
+	if (!p) {
+		p = VG_(cli_malloc)( req_alignB, req_szB );
+		if (!p) {
+			return NULL;
+		}
+		if (is_zeroed) VG_(memset)(p, 0, req_szB);
+		actual_szB = VG_(malloc_usable_size)(p);
+		tl_assert(actual_szB >= req_szB);
+		slop_szB = actual_szB - req_szB;
+	} else {
+		slop_szB = 0;
+	}
+	
+	return p;
+}
+
+
+//------------------------------------------------------------//
+//--- malloc() et al replacement wrappers                  ---//
+//------------------------------------------------------------//
+
+static void* mm_malloc ( ThreadId tid, SizeT szB )
+{
+	VG_(printf)("Allocating %u bytes.",szB);
+	return new_block( tid, NULL, szB, VG_(clo_alignment), /*is_zeroed*/False );
+}
+
+static void* mm___builtin_new ( ThreadId tid, SizeT szB )
+{
+	VG_(printf)("Allocating new %u bytes.",szB);
+}
+
+static void* mm___builtin_vec_new ( ThreadId tid, SizeT szB )
+{
+}
+
+static void* mm_calloc ( ThreadId tid, SizeT m, SizeT szB )
+{
+}
+
+static void *mm_memalign ( ThreadId tid, SizeT alignB, SizeT szB )
+{
+}
+
+static void mm_free ( ThreadId tid __attribute__((unused)), void* p )
+{
+	VG_(printf)("Freeing.\n");
+}
+
+static void mm___builtin_delete ( ThreadId tid, void* p )
+{
+}
+
+static void mm___builtin_vec_delete ( ThreadId tid, void* p )
+{
+}
+
+static void* mm_realloc ( ThreadId tid, void* p_old, SizeT new_szB )
+{
+}
+
+static SizeT mm_malloc_usable_size ( ThreadId tid, void* p )
+{                                                            
+	return 0;
+}                                                            
+
+
+////////////////// End malloc replacements ////////////////////
 
 static void mm_post_clo_init(void)
 {
@@ -71,7 +175,27 @@ static void mm_pre_clo_init(void)
    VG_(basic_tool_funcs)        (mm_post_clo_init,
                                  mm_instrument,
                                  mm_fini);
+   VG_(needs_libc_freeres)();
+   //VG_(needs_client_requests)     (ms_handle_client_request);
+
+	
    VG_(track_new_mem_mmap)      (mm_mem_mmap);
+	VG_(track_new_mem_startup)      ( mm_new_mem_startup);
+	VG_(track_die_mem_munmap)       ( mm_die_mem_munmap );
+
+
+   VG_(needs_malloc_replacement)  (mm_malloc,
+									mm___builtin_new,
+									mm___builtin_vec_new,
+									mm_memalign,
+									mm_calloc,
+									mm_free,
+									mm___builtin_delete,
+									mm___builtin_vec_delete,
+									mm_realloc,
+									mm_malloc_usable_size,
+									0 );
+	
 
    /* No needs, no core events to track */
 }
